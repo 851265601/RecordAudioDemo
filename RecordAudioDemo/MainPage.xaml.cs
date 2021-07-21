d89692b810d5bc4c2b25c1f05890243a25570ff7
leol <leol@WX-72238-01>
2021-07-21 10:46:28 +0800

    Add project files.

---
+++ RecordAudioDemo/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using MediaManager;
+using Plugin.AudioRecorder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RecordAudioDemo
+{
+    public partial class MainPage : ContentPage
+    {
+		AudioRecorderService recorder;
+		public MainPage()
+        {
+            InitializeComponent();
+
+			recorder = new AudioRecorderService
+			{
+				StopRecordingOnSilence = true, //will stop recording after 2 seconds (default)
+				StopRecordingAfterTimeout = true,  //stop recording after a max timeout (defined below)
+				TotalAudioTimeout = TimeSpan.FromSeconds(60) //audio will stop recording after 15 seconds
+			};
+
+
+			
+		}
+
+		async void RecordButton_Click(object sender, EventArgs e)
+		{
+			await RecordAudio();
+		}
+
+		async Task RecordAudio()
+		{
+			try
+			{
+				if (!recorder.IsRecording)
+				{
+					await recorder.StartRecording();
+				}
+				else
+				{
+					await recorder.StopRecording();
+
+					
+				}
+			}
+			catch (Exception ex)
+			{
+				//...
+	         }
+
+			
+		}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+			//var audioFile = await recorder;
+
+			if (recorder.FilePath != null) //non-null audioFile indicates audio was successfully recorded
+			{
+				//do something with the file
+				var path=recorder.FilePath;
+
+				await CrossMediaManager.Current.Play("file://"+ path);
+
+			}
+		}
+    }
+}
